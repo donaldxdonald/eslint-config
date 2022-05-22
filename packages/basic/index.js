dc5c370a38641e952600ff3178d6ae41bcb0138e
donaldxdonald <mzhlovefama@gmail.com>
2022-05-22 14:04:24 +0800

    perf: update deps and some config

--- packages/basic/index.js
+++ packages/basic/index.js
@@ -1,24 +1,25 @@
 module.exports = {
-	env: {
-		browser: true,
-		es2021: true,
-		node: true
-	},
-	extends: [
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
     'standard',
     'plugin:import/recommended',
     'plugin:eslint-comments/recommended',
     'plugin:jsonc/recommended-with-jsonc',
     'plugin:yml/standard',
-	],
-	parserOptions: {
-		ecmaVersion: 13,
-		sourceType: 'module'
-	},
+  ],
+  parserOptions: {
+    ecmaVersion: 13,
+    sourceType: 'module',
+  },
   plugins: [
     'html',
+    'promise',
   ],
-	overrides: [
+  overrides: [
     {
       files: ['*.json', '*.json5'],
       parser: 'jsonc-eslint-parser',
@@ -29,8 +30,8 @@ module.exports = {
       },
     },
     {
-      files: ["*.yaml", "*.yml"],
-      parser: "yaml-eslint-parser",
+      files: ['*.yaml', '*.yml'],
+      parser: 'yaml-eslint-parser',
     },
     {
       files: ['*.d.ts'],
@@ -50,103 +51,103 @@ module.exports = {
         'no-console': 'off',
       },
     },
-	],
+  ],
   settings: {
     'import/resolver': {
       node: { extensions: ['.js', '.mjs', '.ts', '.d.ts'] },
     },
   },
-	rules: {
-		// import
-		'import/order': 'error',
-		'import/first': 'error',
-		'import/no-mutable-exports': 'error',
-		'import/no-unresolved': 'off',
-		'import/no-absolute-path': 'off',
+  rules: {
+    // import
+    'import/order': 'error',
+    'import/first': 'error',
+    'import/no-mutable-exports': 'error',
+    'import/no-unresolved': 'off',
+    'import/no-absolute-path': 'off',
 
-		// Common
-		'semi': ['error', 'never'],
-    "curly": ["error", "all"],
-		'quotes': ['error', 'single'],
-		'quote-props': ['error', 'consistent-as-needed'],
-		'no-unused-vars': 'warn',
-		'no-param-reassign': 'off',
-		'array-bracket-spacing': ['error', 'never'],
-		'brace-style': ['error', 'stroustrup', { allowSingleLine: true }],
-		'block-spacing': ['error', 'always'],
-		'camelcase': 'off',
-		'comma-spacing': ['error', { before: false, after: true }],
-		'comma-style': ['error', 'last'],
-		'comma-dangle': ['error', 'always-multiline'],
-		'no-constant-condition': 'warn',
-		'no-debugger': 'error',
-    "no-console":"off",
-		'no-cond-assign': ['error', 'always'],
-		'func-call-spacing': ['off', 'never'],
-		'key-spacing': ['error', { beforeColon: false, afterColon: true }],
-		'indent': ['error', 2, { SwitchCase: 1, VariableDeclarator: 1, outerIIFEBody: 1 }],
-		'no-restricted-syntax': [
-			'error',
-			'DebuggerStatement',
-			'ForInStatement',
-			'LabeledStatement',
-			'WithStatement',
-		],
-		'object-curly-spacing': ['error', 'always'],
-		'no-return-await': 'off',
-		'space-before-function-paren': ['error', 'never'],
+    // Common
+    'semi': ['error', 'never'],
+    'curly': ['error', 'all'],
+    'quotes': ['error', 'single'],
+    'quote-props': ['error', 'consistent-as-needed'],
+    'no-unused-vars': 'warn',
+    'no-param-reassign': 'off',
+    'array-bracket-spacing': ['error', 'never'],
+    'brace-style': ['error', 'stroustrup', { allowSingleLine: true }],
+    'block-spacing': ['error', 'always'],
+    'camelcase': 'off',
+    'comma-spacing': ['error', { before: false, after: true }],
+    'comma-style': ['error', 'last'],
+    'comma-dangle': ['error', 'always-multiline'],
+    'no-constant-condition': 'warn',
+    'no-debugger': 'error',
+    'no-console': 'off',
+    'no-cond-assign': ['error', 'always'],
+    'func-call-spacing': ['off', 'never'],
+    'key-spacing': ['error', { beforeColon: false, afterColon: true }],
+    'indent': ['error', 2, { SwitchCase: 1, VariableDeclarator: 1, outerIIFEBody: 1 }],
+    'no-restricted-syntax': [
+      'error',
+      'DebuggerStatement',
+      'ForInStatement',
+      'LabeledStatement',
+      'WithStatement',
+    ],
+    'object-curly-spacing': ['error', 'always'],
+    'no-return-await': 'off',
+    'space-before-function-paren': ['error', 'never'],
 
-		// es6
-		'no-var': 'error',
-		'prefer-const': [
-			'error',
-			{
-				destructuring: 'any',
-				ignoreReadBeforeAssign: true,
-			},
-		],
-		'prefer-arrow-callback': [
-			'error',
-			{
-				allowNamedFunctions: false,
-				allowUnboundThis: true,
-			},
-		],
-		'object-shorthand': [
-			'error',
-			'always',
-			{
-				ignoreConstructors: false,
-				avoidQuotes: true,
-			},
-		],
-		'prefer-rest-params': 'error',
-		'prefer-spread': 'error',
-		'prefer-template': 'error',
-		'template-curly-spacing': 'error',
-		'arrow-parens': ['error', 'as-needed'],
-		'generator-star-spacing': 'off',
+    // es6
+    'no-var': 'error',
+    'prefer-const': [
+      'error',
+      {
+        destructuring: 'any',
+        ignoreReadBeforeAssign: true,
+      },
+    ],
+    'prefer-arrow-callback': [
+      'error',
+      {
+        allowNamedFunctions: false,
+        allowUnboundThis: true,
+      },
+    ],
+    'object-shorthand': [
+      'error',
+      'always',
+      {
+        ignoreConstructors: false,
+        avoidQuotes: true,
+      },
+    ],
+    'prefer-rest-params': 'error',
+    'prefer-spread': 'error',
+    'prefer-template': 'error',
+    'template-curly-spacing': 'error',
+    'arrow-parens': ['error', 'as-needed'],
+    'generator-star-spacing': 'off',
 
-		// best-practice
-		'array-callback-return': 'error',
-		'block-scoped-var': 'error',
-		'consistent-return': 'off',
-		'complexity': ['off', 11],
-		'eqeqeq': ['error', 'smart'],
-		'no-alert': 'warn',
-		'no-case-declarations': 'error',
-		'no-multi-spaces': 'error',
-		'no-multi-str': 'error',
-		'no-with': 'error',
-		'no-void': 'error',
-		'no-useless-escape': 'off',
-		'vars-on-top': 'error',
-		'require-await': 'off',
-		'no-return-assign': 'off',
-		'operator-linebreak': ['error', 'before'],
-    "no-tabs": "off",
+    // best-practice
+    'array-callback-return': 'error',
+    'block-scoped-var': 'error',
+    'consistent-return': 'off',
+    'complexity': ['off', 11],
+    'eqeqeq': ['error', 'smart'],
+    'no-alert': 'warn',
+    'no-case-declarations': 'error',
+    'no-multi-spaces': 'error',
+    'no-multi-str': 'error',
+    'no-with': 'error',
+    'no-void': 'error',
+    'no-useless-escape': 'off',
+    'vars-on-top': 'error',
+    'require-await': 'off',
+    'no-return-assign': 'off',
+    'operator-linebreak': ['error', 'before'],
+    'no-tabs': 'off',
 
     'no-use-before-define': ['error', { functions: false, classes: false, variables: true }],
     'eslint-comments/disable-enable-pair': 'off',
-	}
+  },
 }
