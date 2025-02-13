83ff1381ed5a893fb4d65ca9335a4de9c5c3d124
donaldxdonald <mzhlovefama@gmail.com>
2025-02-13 20:46:37 +0800

    chore: update deps

--- packages/config/tsup.config.ts
+++ packages/config/tsup.config.ts
@@ -4,6 +4,7 @@ export default defineConfig({
   tsconfig: '../../tsconfig.json',
   entry: ['src/index.ts'],
   format: ['esm', 'cjs'],
+  target: 'es2022',
   outDir: 'dist',
   clean: true,
   dts: true,
